6bf48b3ac6d12672967ad7c68865f63f9b7fc1de
Junini Florez <juniniprogramador@outlook.com>
2023-10-18 22:58:49 -0500

    feat: added firts mainly modules structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { HotelsModule } from './hotels/hotels.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, HotelsModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
