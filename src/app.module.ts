d8c6fcaf26aa4e09d8f1c579681f9a68a5891f5a
Junini Florez <juniniprogramador@outlook.com>
2023-10-18 23:24:34 -0500

    feat: create auth base strucuture

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { HotelsModule } from './hotels/hotels.module';
 import { ReservationsModule } from './reservations/reservations.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, HotelsModule, ReservationsModule],
+  imports: [UsersModule, HotelsModule, ReservationsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
