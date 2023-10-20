813b1bfff80a1f888604c2ecbb2d7a99e1777e1c
Junini Florez <juniniprogramador@outlook.com>
2023-10-20 18:41:15 -0500

    feat: added user entity maped typeorm

---
+++ src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ unique: true })
+  usename: string;
+  @Column()
+  password: string;
+  @CreateDateColumn()
+  createAt: Date;
+}
