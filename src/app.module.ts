5cd5b744ecf1fe30263bc40fbd34bc8c060b5ae1
Junini Florez <juniniprogramador@outlook.com>
2023-10-20 17:54:48 -0500

    feat: added typeorm base configuration for posgrest

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,25 @@ import { UsersModule } from './users/users.module';
 import { HotelsModule } from './hotels/hotels.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, HotelsModule, ReservationsModule, AuthModule],
+  imports: [
+    UsersModule,
+    HotelsModule,
+    ReservationsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'development',
+      password: 'development',
+      database: 'hotels',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Only for development. Set to false in production.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
